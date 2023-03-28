c18599a37b7f35544e74c63b133a79aa9cea897e
ariinaa <97937396+ariinaa@users.noreply.github.com>
2023-03-28 08:47:19 +0300

    Add files via upload

--- script1.js
+++ script1.js
@@ -2,5 +2,20 @@ const plus = document.querySelector(".js.plus")
 
 const input = document.querySelector(".js.input")
 
-console.log(plus)
-console.log(input)
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+console.log(Add(2,3)),.
